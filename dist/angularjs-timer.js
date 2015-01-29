ae9849520273d1476529222a12c86d4f03d0e79f
superalligator <amoros.louis@gmail.com>
2015-01-28 21:25:57 -0600

    mv timereadable function as filter module

--- dist/angularjs-timer.js
+++ dist/angularjs-timer.js
@@ -1,5 +1,5 @@
 /**
- * angularjs-timer - v0.0.0 - 2015-01-28 9:19 PM
+ * angularjs-timer - v0.0.0 - 2015-01-28 9:25 PM
  * https://github.com/louisamoros/angular-timer
  *
  * Copyright (c) 2015 Louis Amoros
@@ -29,41 +29,46 @@ angular
           }
         };
 
-        function convert(seconds) {
-          var day, format, hour, minute, month, week, year;
-          seconds = parseInt(seconds, 10);
-          minute = 60;
-          hour = minute * 60;
-          day = hour * 24;
-          week = day * 7;
-          year = day * 365;
-          month = year / 12;
-          format = function(number, string) {
-            string = number === 1 ? string : "" + string + "s";
-            return "" + number + " " + string;
-          };
-          switch (false) {
-            case (seconds >= minute):
-              return format(seconds, 'second');
-              case (seconds >= hour):
-                return format(Math.floor(seconds / minute), 'minute');
-                case (seconds >= day):
-                  return format(Math.floor(seconds / hour), 'hour');
-                  case (seconds >= week):
-                    return format(Math.floor(seconds / day), 'day');
-                    case (seconds >= month):
-                      return format(Math.floor(seconds / week), 'week');
-                      case (seconds >= year):
-                        return format(Math.floor(seconds / month), 'month');
-                        default:
-                          return format(Math.floor(seconds / year), 'year');
-                        }
-                      }
 
-                    }
-                  };
-                }
-              );
+      }
+    };
+  }
+)
+.filter(
+  'readableTime',
+   function() {
+  return function ( seconds ) {
+    var day, format, hour, minute, month, week, year;
+    seconds = parseInt(seconds, 10);
+    minute = 60;
+    hour = minute * 60;
+    day = hour * 24;
+    week = day * 7;
+    year = day * 365;
+    month = year / 12;
+    format = function(number, string) {
+      string = number === 1 ? string : "" + string + "s";
+      return "" + number + " " + string;
+    };
+    switch (false) {
+      case (seconds >= minute):
+        return format(seconds, 'second');
+        case (seconds >= hour):
+          return format(Math.floor(seconds / minute), 'minute');
+          case (seconds >= day):
+            return format(Math.floor(seconds / hour), 'hour');
+            case (seconds >= week):
+              return format(Math.floor(seconds / day), 'day');
+              case (seconds >= month):
+                return format(Math.floor(seconds / week), 'week');
+                case (seconds >= year):
+                  return format(Math.floor(seconds / month), 'month');
+                  default:
+                    return format(Math.floor(seconds / year), 'year');
+                  }
+                };
+              }
+            );
 
 // 'use strict';
 
