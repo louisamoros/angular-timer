ac5cd37066b6ce28e1342a4f5dc146185ba783b0
superalligator <amoros.louis@gmail.com>
2015-01-28 21:04:14 -0600

    remove git config only angularjs directive/service load in angular module

--- examples/app.js
+++ examples/app.js
@@ -1,29 +1,10 @@
 'use strict';
-
 /**
-* # angularApp
-*
+* # angularjsTimerExample
 * Main module of the application.
 */
 angular
 .module('angularjsTimerExample', [
-'ngRoute',
 'angularjs-timer-service',
 'angularjs-timer-directive'
-]).
-config(['$routeProvider',function($routeProvider) {
-  $routeProvider.when("/", {
-    templateUrl: "examples/timer/timer.html",
-    controller: "TimerController"
-  });
-  $routeProvider.when("/timers", {
-    templateUrl: "examples/timers/timers.html",
-    controller: "TimersController"
-  });    $routeProvider.when("/countdown", {
-    templateUrl: "examples/countdown/countdown.html",
-    controller: "CountdownController"
-  });
-  $routeProvider.otherwise({
-    redirectTo: '/'
-  });
-}])
+])
